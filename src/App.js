41b0079d254eec657a7b0692a1ca9848bb5ec5d2
Monique <moniquedgomes@outlook.com>
2024-06-14 19:21:33 -0300

    criação das pages e configuraçao da navegaçao entre paginas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
 
-function App() {
+const App = () => {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl">Teste site</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Sobre Nós</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contato</Link>
+            </li>
+            <li>
+              <Link to="/services">Serviços</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Sobre Nós</h1>
+      <p>Informações sobre a Horizon Engenharia.</p>
+    </div>
+  );
+};
+
+export default About;
